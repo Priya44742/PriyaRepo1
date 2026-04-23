a1f8c572b4a5e160d41485fc3cd4c30b8ebcdf66
Priya <priyapatil6393@gmail.com>
2026-04-23 13:23:47 +0530

    M1 method called in Test1 main method

--- JenkinsDemo23/src/com/Test1.java
+++ JenkinsDemo23/src/com/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) 
 	{
+		Test1 t = new Test1();
+		t.m1();
+		
 		System.out.println("Hello java");
         System.out.println("New Line added to Test1 class");
 	}
